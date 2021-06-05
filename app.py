481b45eecd4c1f0f34d4f2fc16e608609b03a546
Manasa Jagadeesh <manasajagadeesh98@gmail.com>
2021-06-05 20:06:49 +0530

    Notion collections

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 def notionConnect():
     client = NotionClient(token_v2="108f91fb55af7f8f944bae8d3a5db8adbf55f4c43e81227e92beae5f83aadaa7b8663da0dee2b417bd20d665f87852cb672d5adcf003b4930656899f22f0a32c294cd0ca8072235e549df37b4fc6")
-    page = client.get_block("https://www.notion.so/4c884665930e48d3bd2e76d4ff13084c?v=9bb58e513a554446b7b0e7e9b4d74f17")
+    page = client.get_collection_view("https://www.notion.so/a2ba916475ab4760a6e382d2edaa0674?v=5454c4fc1cb140eb8c573a3921ecc2bf")
     return page 
 
 
@@ -16,12 +16,14 @@ def bot():
     resp = MessagingResponse()
     msg = resp.message()    
     page = notionConnect()
-    for child in page.children:
-        response = child.title
+    ct = 0 
+    for obj in page.collection.get_rows():
+        print(obj.title)
+        ct+=1
 
     responded = False
     if 'hello' in incoming_msg:
-        msg.body("This is "+response)
+        msg.body("There are "+ct+" puzzles available")
         responded = True
     if 'bye' in incoming_msg:
         msg.body("Sayonara")
